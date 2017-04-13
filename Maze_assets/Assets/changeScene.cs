42fe8945f23ca68a61b3f58ffee42c742208352b
Muriel Gonzalez <muriel_gonzalez@muriel.local>
2017-04-12 22:53:32 -0500

    p3 prep

---
+++ Maze_assets/Assets/changeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeScene : MonoBehaviour {
+	public void GoToScene(string sceneName) {
+		SceneManager.LoadScene (sceneName);
+		//SceneManager.LoadScene(0);
+	}
+
+}
